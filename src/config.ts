b88eba1c6a39dc566a9e6063076aa9acdea3f9f9
lin0315 <1845948316@qq.com>
2024-06-29 22:52:22 +0800

    feat: tank move

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,10 @@ import straw from './static/images/straw/straw.png'
 import wall from './static/images/wall/wall.gif'
 import water from './static/images/water/water.gif'
 import steel from './static/images/wall/steels.gif'
-import tank from './static/images/tank/bottom.gif'
+import tankTop from './static/images/tank/top.gif'
+import tankRight from './static/images/tank/right.gif'
+import tankBottom from './static/images/tank/bottom.gif'
+import tankLeft from './static/images/tank/left.gif'
 
 export default {
   canvas: {
@@ -40,7 +43,10 @@ export default {
     wall,
     water,
     steel,
-    tank
+    tankTop,
+    tankRight,
+    tankBottom,
+    tankLeft
   }
 
 
--- src/model/Tank.ts
+++ src/model/Tank.ts
@@ -1,9 +1,50 @@
+import _ from 'lodash'
+import { directionEnum } from "../enum/directionEnum";
 import { images } from "../server/image";
 import ModelAbstract from "./ModelAbstract";
+import config from '../config';
+
 
 export default class Tank extends ModelAbstract implements IModel {
+  name: string = 'tank'
+
+  protected direction: directionEnum = directionEnum.top
   render(): void {
-    super.draw(images.get('tank')!)
+    this.randomDirection()
+    super.draw(this.randomImage())
+
+    setInterval(() => this.move(), 50)
+  }
+
+  protected move(): void {
+    this.canvas.clearRect(this.x, this.y, config.model.width, config.model.height)
+    switch (this.direction) {
+      case directionEnum.top:
+        this.y -= 2;
+        break;
+      case directionEnum.right:
+        this.x += 2;
+        break;
+      case directionEnum.bottom:
+        this.y += 2;
+        break;
+      case directionEnum.left:
+        this.x -= 2;
+        break;
+      default:
+        break;
+    }
+    super.draw(this.randomImage())
+  }
+
+  // 获取随机方向
+  randomDirection() {
+    this.direction = Object.keys(directionEnum)[Math.floor(Math.random() * 4)] as directionEnum
+  }
+
+  randomImage() {
+    let direction = this.name + _.upperFirst(this.direction)
+    return images.get(direction as keyof typeof config.images)!
   }
 
 }
